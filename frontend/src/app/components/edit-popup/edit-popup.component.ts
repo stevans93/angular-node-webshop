a6c6627f747f41cfd044ef4703f32f0448d41e3a
stevans93 <stevans93@gmail.com>
2024-10-18 15:09:27 +0200

    done

--- frontend/src/app/components/edit-popup/edit-popup.component.ts
+++ frontend/src/app/components/edit-popup/edit-popup.component.ts
@@ -2,7 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { Product } from '../../../types';
-import { FormBuilder, FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
 import { ButtonModule } from 'primeng/button';
 
@@ -15,12 +21,16 @@ import { ButtonModule } from 'primeng/button';
     FormsModule,
     RatingModule,
     ButtonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './edit-popup.component.html',
   styleUrl: './edit-popup.component.scss',
 })
 export class EditPopupComponent {
+  productForm: any;
+
   constructor(private formBuilder: FormBuilder) {}
+
   @Input() display: boolean = false;
   @Output() displayChange = new EventEmitter<boolean>();
   @Output() confirm = new EventEmitter<Product>();
@@ -34,8 +44,29 @@ export class EditPopupComponent {
     rating: 0,
   };
 
+  specialCharacterValidator(): ValidatorFn {
+    return (control) => {
+      const hasSpecialCharacter = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(
+        control.value
+      );
+
+      return hasSpecialCharacter ? { hasSpecialCharacter: true } : null;
+    };
+  }
+
+  ngOnChanges() {
+    this.productForm = this.formBuilder.group({
+      name: ['', [Validators.required, this.specialCharacterValidator()]],
+      price: ['', [Validators.required]],
+      rating: [0],
+      image: [''],
+    });
+
+    this.productForm.patchValue(this.product);
+  }
+
   onConfirm() {
-    this.confirm.emit(this.product);
+    this.confirm.emit(this.productForm.value);
     this.display = false;
     this.displayChange.emit(this.display);
   }
